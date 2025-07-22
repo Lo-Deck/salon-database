e4392d7621e89bd4f37f0b16e353f1b5b2b06f86
Lo-Deck <122597745+Lo-Deck@users.noreply.github.com>
2025-07-22 13:20:17 +0200

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=salon -t --no-align -c"
+
 echo -e "\n~~~New Salon In Town~~~"
 
 MAIN_MENU () {
@@ -10,46 +12,46 @@ MAIN_MENU () {
   fi
 
 
-  echo -e "\nChoose your new haircut"
-
-  echo -e "\n1) Haircut Man\n2) Haircut Woman\n3) Haircut Kid\n"
+  SERVICES_LIST=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id;") 
+  echo "$SERVICES_LIST" | while IFS="|" read SERVICE_ID SERVICE_NAME;
+  do
+    echo "$SERVICE_ID) $SERVICE_NAME"
+  done
 
   read SERVICE_ID_SELECTED
 
-  case $SERVICE_ID_SELECTED in
-    1)MEN;;
-    2)WOMEN;;
-    3)KID;;
-    4)EXIT;;
-    *)MAIN_MENU "Invalid choice. Please enter 1, 2, 3";; 
-  esac
+  if [[ -z $SERVICE_ID_SELECTED || ! $SERVICE_ID_SELECTED =~ ^[1-4]$ ]]
+  then
+    MAIN_MENU "Ce n'est pas un numéro de service valide."
+  else
+    SERVICE_MENU "$SERVICE_ID_SELECTED" "$SERVICE_NAME"
+  fi
 
-  # if [[ -z $SERVICE_CHOICE ]]
-  # then
-  #   MAIN_MENU "Make a choice. Please enter 1, 2, 3"
-  # else
-  # fi  
+
+
+}
+
+SERVICE_MENU () {
 
     echo -e "\nPlease enter your phone number"
     read CUSTOMER_PHONE
 
     if [[ $CUSTOMER_PHONE =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$ ]] 
     then
-      CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
 
+      CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
       if [[ -z $CUSTOMER_NAME ]]
       then
         echo -e "\nCreate New Customer"
         echo -e "\nEnter your name"
         read CUSTOMER_NAME
-        $($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+        INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
       fi
 
     else
       MAIN_MENU "Wrong phone number use format (555-555-5555)"
-    fi    
+    fi  
 
-    # Customer_name = OK => Create appointment
 
     CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE name='$CUSTOMER_NAME'")
 
@@ -64,29 +66,101 @@ MAIN_MENU () {
       MAIN_MENU "Please enter a date for your appointment format(10:30)"
     fi
 
+    # SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = '$SERVICE_ID_SELECTED'")
 
-
+    echo -e "/nI have put you down for a '$SERVICE_NAME' at '$SERVICE_TIME', '$SERVICE_NAME'."
 
 
 
 }
 
-MEN() {
 
-}
+  # do
+  #   echo -e "\nChoose your new haircut"
+  #   echo -e "\n1) Haircut Man\n2) Haircut Woman\n3) Haircut Kid\n4) Exit\n"
+  #   read SERVICE_CHOICE
+  #   if [[ -z $SERVICE_CHOICE || ! $SERVICE_CHOICE =~ ^[1-4]$ ]]
+  #   then
+  #     MAIN_MENU "Make a choice. Please enter 1, 2, 3"
+  #   else
+  #   fi  
+  # done
+  
 
-WOMEN() {
+  # DISPLAY_MENU
 
-}
+  # read SERVICE_ID_SELECTED
 
-KID() {
-
-}
-
-EXIT() {
-  echo -e "\nThank you for visiting New Salon In Town. Goodbye!\n"
-  exit 0
-}
+  # case $SERVICE_ID_SELECTED in
+  #   1)$SERVICE_ID_SELECTED;;
+  #   2)$SERVICE_ID_SELECTED;;
+  #   3)$SERVICE_ID_SELECTED;;
+  #   *)MAIN_MENU "Invalid choice. Please enter 1, 2, 3";; 
+  # esac
 
 
+  # read SERVICE_CHOICE
+  # if [[ -z $SERVICE_CHOICE || ! $SERVICE_CHOICE =~ ^[1-4]$ ]]
+  # then
+  #   MAIN_MENU "Make a choice. Please enter 1, 2, 3"
+  # else
+  # fi  
+  
 
+  #   # Customer_name = OK => Create appointment
+
+  #   CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE name='$CUSTOMER_NAME'")
+
+  #   echo -e "\nPlease enter a date for your appointment format(10:30)"
+
+  #   read SERVICE_TIME
+
+  #   if [[ $SERVICE_TIME =~ ^[0-9]{2}:[0-9]{2}$ ]]
+  #   then
+  #     $($PSQL "INSERT INTO appointments(customer_id, time) VALUES('$CUSTOMER_ID', '$SERVICE_TIME')")
+  #   else
+  #     MAIN_MENU "Please enter a date for your appointment format(10:30)"
+  #   fi
+
+  #   SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = '$SERVICE_ID_SELECTED'")
+
+  #   echo -e "/nI have put you down for a '$SERVICE_NAME' at '$SERVICE_TIME', '$SERVICE_NAME'."
+
+
+
+
+
+
+# DISPLAY_MENU() {
+
+#   if [[ $1 ]]
+#   then
+#     echo -e "\n$1"
+#   fi
+
+#   SERVICES_LIST=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id;") 
+#   echo "$SERVICES_LIST" | while IFS="|" read SERVICE_ID SERVICE_NAME;
+#   do
+#     echo "$SERVICE_ID) $SERVICE_NAME"
+#   done
+#   echo -e "4) exit"
+# }
+
+# WOMEN() {
+
+# }
+
+# KID() {
+
+# }
+
+# EXIT() {
+#   echo -e "\nThank you for visiting New Salon In Town. Goodbye!\n"
+#   exit 0
+# }
+
+
+
+
+
+MAIN_MENU
